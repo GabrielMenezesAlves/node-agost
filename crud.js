ecd60ba9cd7bde6bc5c55cef341769554a85d0ff
Gabriel Menezes Alves <gabriel_cm15@hotmail.com>
2024-08-22 20:05:36 -0300

    crud

--- crud.js
+++ crud.js
@@ -4,3 +4,21 @@
 // Update -> atualizacao
 // Delete -> apagar
 
+const { MongoClient } = require("mongodb");
+
+const fs = require("fs");
+
+const urlcon = "mongodb+srv://aula-back:aula123456@turma-agosto.mam4s.mongodb.net/"
+
+const conexao = new MongoClient(urlcon);
+
+async function lerDados()
+{
+    let db = await conexao.connect();
+    let pasta = db.db("gabriel").collection("contatos");
+    let dados = await pasta.find({}).toArray();
+
+    console.log(dados);
+}
+
+lerDados();
